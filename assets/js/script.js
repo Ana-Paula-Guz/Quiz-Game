2e6b09bddc4ac1c92ef251f060201eba9db0d31b
Ana-Paula-Guz <>
2024-01-19 16:42:22 +0000

    Create an array of objects for the user options

---
+++ assets/js/script.js
@@ -0,0 +1,97 @@
+const questions= [
+    {
+        question: "How many counties are in the Republic of Ireland?"
+        answers: [
+            { text: "26", correct: true };
+            { text: "30", correct: false };
+            { text: "35", correct: false };
+        ]
+    };
+    {
+        question: "What is the capital of Estonia? "
+        answers: [
+            { text: "Tallinn", correct: true };
+            { text: "Helsinki", correct: false };
+            { text: "Riga", correct: false };
+        ]
+    };
+    {
+        question: "Broome is a town in which state of Australia?"
+        answers: [
+            { text: "Western Australia", correct: true };
+            { text: "Northern Territory", correct: false };
+            { text: "Tasmania", correct: false };
+        ]
+    };
+    {
+        question: "What was the most populous city in the Americas in 2015?"
+        answers: [
+            { text: "Sao Paulo", correct: true };
+            { text: "New York", correct: false };
+            { text: "Mexico", correct: false };
+        ];
+    {;
+    {
+        question: "What was the most populous city in the Americas in 2015?";
+        answers: [
+            { text: "Sao Paulo", correct: true };
+            { text: "New York", correct: false };
+            { text: "Mexico", correct: false };
+        ];
+    }; 
+    {
+        question: "How many countries share a land border with Luxembourg?"
+        answers: [
+            { text: "3", correct: true };
+            { text: "5", correct: false };
+            { text: "4", correct: false };
+        ];
+    }; 
+    {
+        question: "Which of the following is NOT a capital city?"
+        answers: [
+            { text: "Sydney", correct: true };
+            { text: "Cairo", correct: false };
+            { text: "Moscow", correct: false };
+        ];
+    }; 
+   {
+        question: "How many countries does Spain have a land border with?"
+        answers: [
+            { text: "5", correct: true };
+            { text: "4", correct: false };
+            { text: "3", correct: false };
+        ];
+   };
+        {
+            question: "Which of these is NOT a city in Saudi Arabia?"
+            answers: [
+                { text: "Dubai", correct: true };
+                { text: "Mecca", correct: false };
+                { text: "Medina", correct: false };
+            ];
+    };
+    {
+            question: "If you travelled to the city of Volgograd, which country would be in?"
+            answers: [
+                { text: "Russia", correct: true };
+                { text: "Poland", correct: false };
+                { text: "Romania", correct: false };
+            ];
+    };   
+]
+
+
+
+function startQuiz() {
+
+}
+function showQuestion() {
+    function showOptions(){
+        if true/false
+           green or red
+    };
+};
+function nextQuestion() {
+    function showScore() 
+}
