98b45e1f88b1713af9dfe11cd3c42edd6d071d47
Ana-Paula-Guz <>
2024-01-26 22:34:22 +0000

    Add an Event Listener to buttons elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 /**Create my variables */
-let correctAnswer = [26, Tallinn, 'Western Australia', 'Sao Paulo', 3, Sydney, 5, Dubai, Russia, Servern];
-let answerChosen= [];
-let score= 0;
+let correctAnswers = [26, 'Tallinn', 'Western Australia', 'Sao Paulo', 3, 'Sydney', 5, 'Dubai', 'Russia', 'Servern'];
+let answerChosen = [];
+let score = 0;
+
+
+let buttons = document.getElementsByClassName("btn");
+
+
+for (let btn of buttons) {
+    btn.addEventListener("click", function () {
+        if (this.getAttribute("data-type") === "true") {
+            btn.style.backgroundColor = "green";
+        } else {
+            btn.style.backgroundColor = "red";
+        }
+    });
+}
 
 
 
