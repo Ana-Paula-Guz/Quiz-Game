c910957bc217ca08680aa8bce54df3dd9797c91e
Ana-Paula-Guz <>
2024-01-26 14:40:45 +0000

    Create basic variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+/**Create my variables */
+let correctAnswer = [26, Tallinn, 'Western Australia', 'Sao Paulo', 3, Sydney, 5, Dubai, Russia, Servern];
+let answerChosen= [];
+let score= 0;
+
+
+
+
+
+
+
+
 
 
